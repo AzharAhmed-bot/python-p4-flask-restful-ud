ec18e1f30014101e58e9a51573a067bdc93c931f
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-07-07 08:21:30 +0300

    Complete assignment

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response,jsonify
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -78,6 +78,24 @@ class NewsletterByID(Resource):
             200,
         )
 
+        return response
+    
+    def patch(self,id):
+        record=Newsletter.query.filter_by(id=id).first()
+        for attribute in request.form:
+            setattr(record,attribute,request.form[attribute])
+        db.session.add(record)
+        db.session.commit()
+        response_dict=record.to_dict()
+        response=make_response(jsonify(response_dict),200)
+        return response
+    
+    def delete(self,id):
+        record=Newsletter.query.filter_by(id=id).first()
+        db.session.delete(record)
+        db.session.commit()
+        response_dict={"message": "record successfully deleted"}
+        response=make_response(jsonify(response_dict),200)
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
